2a601be4db5bb49ad6738f1ef60c9ccf6d01b3f0
Viktor Sharha <vsharha07@gmail.com>
2026-02-09 16:02:32 +0000

    optimise playwright script

--- notebooklm_sources/upload_sources.py
+++ notebooklm_sources/upload_sources.py
@@ -4,7 +4,11 @@ from patchright.sync_api import sync_playwright
 def upload_files(files: list[Path | str], page):
     files: list[Path] = [Path(file) for file in files]
 
+    page.wait_for_selector(".add-source-button")
+
     page.locator(".add-source-button").click()
+
+    page.wait_for_selector(".drop-zone")
 
     with page.expect_file_chooser() as fc_info:
         page.locator(".drop-zone").get_by_text("Upload files").click()
@@ -13,6 +17,8 @@ def upload_files(files: list[Path | str], page):
     file_chooser.set_files(files)
 
 def list_uploaded(page) -> list[str]:
+    page.wait_for_load_state("domcontentloaded")
+
     return [
         source.locator(".source-title-column").text_content()
         for source in page.locator(".single-source-container").all()
@@ -26,13 +32,14 @@ def upload_sources(notebook_url: str, files: list[Path | str]):
         context = playwright.chromium.launch_persistent_context(
             user_data_dir="profile",
             headless=False,
+            args=["--disable-blink-features=AutomationControlled"],
         )
         page = context.pages[0] if context.pages else context.new_page()
 
         page.goto(notebook_url)
-        if page.url != notebook_url:
+        if not page.url.startswith(notebook_url):
             print("Waiting for authentication...")
-            page.wait_for_url(notebook_url, timeout=2 * 60 * 1000)
+            page.wait_for_url(f"{notebook_url}**", timeout=2 * 60 * 1000)
 
         already_uploaded = set(list_uploaded(page))
         new_files = [
