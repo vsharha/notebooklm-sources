248588010e4a09867623d13e78a78d4e02fa4ec0
Viktor Sharha <vsharha07@gmail.com>
2026-02-12 18:33:53 +0000

    add mappings for Y2 courses

--- notebooklm_sources/mapping.py
+++ notebooklm_sources/mapping.py
@@ -20,5 +20,23 @@ mapping: dict[str, CourseConfig] = {
             "lecture": "lecture-*",
         },
         "notebook_url": "https://notebooklm.google.com/notebook/5e8b1c41-babf-456c-a47c-b5f3b85528ef",
+    },
+    "inf2-iads": {
+        "sources": {
+            "url": "https://opencourse.inf.ed.ac.uk/inf2-iads/course-materials/semester-1/schedule"
+            # sem 2 https://opencourse.inf.ed.ac.uk/inf2-iads/course-materials/semester-2/schedule
+        }
+    },
+    # "inf2-sepp": {
+    #     "sources": {
+    #         "url": "https://opencourse.inf.ed.ac.uk/inf2-sepp/schedule"
+    #     }
+    # },
+    "inf2-fds": {
+        "sources": {
+            "url": "https://opencourse.inf.ed.ac.uk/inf2-fds/course-materials/",
+            "week": "semester-*",
+            "lecture": "week-*"
+        }
     }
 }
